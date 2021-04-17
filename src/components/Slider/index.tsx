2a4c5092bcaafc83aee738e08a640b9c94d7649c
Williams25 <william.souza.timao@hotmail.com>
2021-04-16 21:18:25 -0300

    Ajustes no component slider

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,15 +1,114 @@
 import { useState } from 'react'
 import { ArrowBackCircleOutline, ArrowForwardCircleOutline } from 'react-ionicons'
-import { ButtonLeft, ButtonRight, Img, Slider } from './styles'
+import styled from 'styled-components'
 
-interface images {
+const Slider = styled.div`
+  width: 100%;
+  height: 15rem;
+  display: flex;
+  justify-content: center;
+  position: relative;
+
+  /* div {
+    position: absolute;
+    bottom: 0;
+    z-index: 5;
+  }
+
+  div span {
+    margin: 1rem;
+    width: 15px;
+    height: 15px;
+    border: 2px solid #fff;
+    border-radius: 15px;
+    cursor: pointer;
+  } */
+`
+
+const Img = styled.img`
+  width: 100%;
+  height: 15rem;
+  position: absolute;
+  backface-visibility: hidden;
+  border-radius: 8px 8px 0 0;
+  transition: 5s ease-in-out;
+  z-index: 2;
+`
+
+const ButtonRight = styled.button`
+    position: absolute;
+    bottom: 5rem;
+    right: 1rem;
+    color: blue;
+    font-size: 3rem;
+    font-weight: bold;
+    outline: none;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+
+    z-index: 5;
+    visibility: hidden;
+    transition: visibility 0.4s;
+
+    ${Slider}:hover & {
+      visibility: unset;
+    }
+`
+
+const ButtonLeft = styled.button`
+    position: absolute;
+    bottom: 5rem;
+    left: 1rem;
+    color: blue;
+    font-size: 3rem;
+    font-weight: bold;
+    outline: none;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+
+    z-index: 5;
+    visibility: hidden;
+    transition: visibility 0.4s;
+
+    ${Slider}:hover & {
+      visibility: unset; 
+    }
+`
+
+const SliderItemSelection = styled.div`
+  position: absolute;
+  bottom: 0;
+  z-index: 5;
+
+  span {
+    margin: 1rem;
+    width: 1rem;
+    height: 1rem;
+    border: 0.13rem solid #fff;
+    border-radius: 1rem;
+    cursor: pointer;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  span div {
+    width: 0.7rem;
+    height: 0.7rem;
+    border-radius: 1rem;
+    cursor: pointer;
+  }
+`
+interface imagesInterface {
   images: {
     description: string;
     path: string;
   }[]
 }
 
-const SliderItem = ({ images }: images) => {
+export const SliderItem = ({ images }: imagesInterface) => {
   const [imagesIndex, setImagesIndex] = useState<number>(0)
   const imagesLength: number = images.length - 1
 
@@ -28,9 +127,7 @@ const SliderItem = ({ images }: images) => {
       setImagesIndex(imagesIndex + 1)
     }
   }
-
-  const tester = (index: number) => setImagesIndex(index)
-
+  const selectIndex = (index: number) => setImagesIndex(index)
   return (
     <>
       <Slider>
@@ -50,22 +147,24 @@ const SliderItem = ({ images }: images) => {
             height="40px" />
         </ButtonRight>
 
-        <div>
+        <SliderItemSelection>
           {
             images.map((image, index) => {
               return (
                 <span key={index}
-                  onClick={() => tester(index)}
-                  style={{
+                  onClick={() => selectIndex(index)}
+                >
+                  <div style={{
                     background: imagesIndex === index ? 'red' : 'transparent'
-                  }} />
+                  }}>
+
+                  </div>
+                </span>
               )
             })
           }
-        </div>
+        </SliderItemSelection>
       </Slider>
     </>
   )
 }
-
-export default SliderItem
